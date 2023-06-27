528235d7d560a11e6eab3c292ab4880a657033a4
geraldo_domingos <geraldoneto@locus.software>
2023-06-27 16:20:37 -0300

    ✨feature: create pages & Link Component

---
+++ pages/404.js
@@ -0,0 +1,12 @@
+import Link from '../src/components/Link'
+
+export default function ErrorPage() {
+  return (
+    <div>
+        <h1>Ocorre um erro!</h1>
+        <Link href="/">
+          voltar para a HOME
+        </Link>
+    </div>
+  )
+}
---
+++ pages/faq.js
@@ -0,0 +1,12 @@
+import Link from '../src/components/Link'
+
+export default function FAQPage() {
+  return (
+    <div>
+        <h1>FAQPage</h1>
+        <Link href="/">
+          Ir para a HOME
+        </Link>
+    </div>
+  )
+}
