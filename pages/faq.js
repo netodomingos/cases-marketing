19eb5cd3e3df641576fd12581d2493065a8ee688
Geraldo Neto <netoh@Geraldo>
2023-07-18 20:08:44 -0300

    ✨feature: font style config

--- pages/faq.js
+++ pages/faq.js
@@ -1,12 +1,10 @@
-import Link from '../src/components/Link'
+// faq.js
+import FAQScreen from '../src/screens/FAQScreen';
 
-export default function FAQPage() {
-  return (
-    <div>
-        <h1>FAQPage</h1>
-        <Link href="/">
-          Ir para a HOME
-        </Link>
-    </div>
-  )
+export default FAQScreen; 
+
+export const getStaticProps = async () => {
+  const FAQ_URL = 'https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json'
+  const faq = await fetch(FAQ_URL).then(response => { return response.json() })
+  return { props: { faq } }
 }
